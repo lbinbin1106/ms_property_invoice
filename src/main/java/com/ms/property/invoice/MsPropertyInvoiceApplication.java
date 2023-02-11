2d9eaf2ca0c693d0495637bd4278b2dd6d4b4c02
Jessica Mo <jessica.mo@192-168-1-111.tpgi.com.au>
2023-02-11 22:39:46 +1100

    initial commit

---
+++ src/main/java/com/ms/property/invoice/MsPropertyInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.property.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPropertyInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPropertyInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/property/invoice/MsPropertyInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.property.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPropertyInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
