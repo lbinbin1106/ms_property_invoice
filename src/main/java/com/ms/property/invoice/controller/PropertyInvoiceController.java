dd61339875ce4ec5e69e4cbc18e8ab05addc87a3
Jessica Mo <jessica.mo@192-168-1-111.tpgi.com.au>
2023-02-26 22:22:39 +1100

    put and create end point

--- src/main/java/com/ms/property/invoice/controller/PropertyInvoiceController.java
+++ src/main/java/com/ms/property/invoice/controller/PropertyInvoiceController.java
@@ -1,16 +1,19 @@
 package com.ms.property.invoice.controller;
 
 import com.ms.property.invoice.api.InvoiceApiResponse;
+import com.ms.property.invoice.entity.PropertyInvoice;
+import com.ms.property.invoice.service.PropertyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -21,11 +24,37 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Slf4j
 public class PropertyInvoiceController {
 
+    private final PropertyService propertyService;
+
     @GetMapping("/v1/invoices")
-    public ResponseEntity<InvoiceApiResponse> getInvoices(
-            @RequestHeader final HttpHeaders requestHeadres
+    public ResponseEntity<List<PropertyInvoice>> getInvoices (
+            @RequestHeader final HttpHeaders requestHeaders
     ) {
-        InvoiceApiResponse invoiceApiResponse = InvoiceApiResponse.builder().invoiceId("0000").build();
-        return new ResponseEntity<>(invoiceApiResponse, HttpStatus.OK);
+        return new ResponseEntity<>(propertyService.findAllInvoice(), HttpStatus.OK);
+    }
+
+    @PostMapping("/v1/invoices")
+    public ResponseEntity<InvoiceApiResponse> postInvoice (
+            @RequestHeader final HttpHeaders requestHeaders,
+            @Valid @RequestBody PropertyInvoice propertyInvoice
+    ) {
+        propertyService.createOrUpdateInvoice(propertyInvoice);
+        InvoiceApiResponse invoiceApiResponse = InvoiceApiResponse.builder().id(propertyInvoice.getId()).build();
+        return new ResponseEntity<>(invoiceApiResponse, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/v1/invoices/{id}")
+    public ResponseEntity<InvoiceApiResponse> putInvoice (
+            @PathVariable(value = "id") Long invoiceTableId,
+            @RequestHeader final HttpHeaders requestHeaders,
+            @Valid @RequestBody PropertyInvoice propertyInvoice
+    ) {
+        PropertyInvoice updatedInvoice = propertyService.findById(invoiceTableId);
+        updatedInvoice.setInvoiceId(propertyInvoice.getInvoiceId());
+        updatedInvoice.setInvoiceValue(propertyInvoice.getInvoiceValue());
+        updatedInvoice.setInvoiceDate(propertyInvoice.getInvoiceDate());
+        updatedInvoice.setInvoiceMemo(propertyInvoice.getInvoiceMemo());
+        propertyService.createOrUpdateInvoice(updatedInvoice);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/ms/property/invoice/entity/PropertyInvoice.java
+++ src/main/java/com/ms/property/invoice/entity/PropertyInvoice.java
@@ -1,21 +1,32 @@
 package com.ms.property.invoice.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import java.util.Date;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.sql.Date;
 
 @Entity
 @Table(name = "property_invoice")
 @Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class PropertyInvoice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotNull
+    @Column(nullable = false)
     private String invoiceId;
-    private Number invoiceValue;
+    @NotNull
+    @Column(nullable = false)
+    private Double invoiceValue;
+    @NotNull
+    @Column(nullable = false)
     private Date invoiceDate;
+    @NotNull
+    @Column(nullable = false)
     private String invoiceMemo;
 }
